647f056b1f3e819d5dcddadaaab6b4e0d98438ba
Romain Guillemot <rocambille@gmail.com>
2023-05-30 14:39:12 +0200

    Initial commit

---
+++ backend/database.sql
@@ -0,0 +1,6 @@
+CREATE TABLE item (
+  id int(11) UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  title varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
---
+++ backend/src/router.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const router = express.Router();
+
+const itemControllers = require("./controllers/itemControllers");
+
+router.get("/items", itemControllers.browse);
+router.get("/items/:id", itemControllers.read);
+router.put("/items/:id", itemControllers.edit);
+router.post("/items", itemControllers.add);
+router.delete("/items/:id", itemControllers.destroy);
+
+module.exports = router;
