b5ae34d424a0bb2a972dbdc64e9c5c5e84fbd6c9
JulienBenoit7 <julien-benoit@hotmail.fr>
2023-05-31 11:05:21 +0200

    header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,11 +3,10 @@ import Logo from "../images/logo RGB Original Digital.png";
 function Header() {
   return (
     <div className="header">
-      <img src={Logo} alt="logo Origins digital" className="headerLogo" />;
+      <img src={Logo} alt="logo Origins digital" className="headerLogo" />
       <button type="button" className="header_button">
         Log In
       </button>
-      ;
     </div>
   );
 }
