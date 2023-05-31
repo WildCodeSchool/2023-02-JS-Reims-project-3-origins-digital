9bc6cc2c53ae3197d06f2e6daf4893bf366e6782
christophe Dubois <dubois51ch@gmail.com>
2023-05-31 15:53:22 +0200

    footer html 2 partie et css + responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import Home from "./pages/Home";
-
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Footer />
     </div>
   );
 }
