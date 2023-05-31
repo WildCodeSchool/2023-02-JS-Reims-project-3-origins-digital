2b4c3279c870a469961323d300f5639e468a5500
MOHAMED MAACH <maach.med@yahoo.com>
2023-05-31 15:21:25 +0200

    Resolve conflict and make text white

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,8 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import CategoryBar from "./components/CategoryBar";
+import CategoryPage from "./components/CategoryPage";
 
 function App() {
   return (
