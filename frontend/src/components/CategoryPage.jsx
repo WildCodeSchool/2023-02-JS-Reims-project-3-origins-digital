18af916e40e88e13c6ca9f8d8b3ec22706311ce0
MOHAMED MAACH <maach.med@yahoo.com>
2023-06-01 15:48:30 +0200

    add picture coming soon

--- frontend/src/components/CategoryPage.jsx
+++ frontend/src/components/CategoryPage.jsx
@@ -5,9 +5,14 @@ function CategoryPage() {
   const { categoryName } = useParams();
 
   return (
-    <div>
+    <div style={{ textAlign: "center" }}>
       <h1>{categoryName} </h1>
       <p>Videos Soon...stay Connected</p>
+      <img
+        src="https://as2.ftcdn.net/v2/jpg/04/75/50/13/1000_F_475501309_7E1uysMf46oZA5FOPwzuqIxgIKjxGsMN.jpg"
+        alt="Work in progress"
+        style={{ width: "500px", height: "auto", margin: "0 auto" }}
+      />
     </div>
   );
 }
