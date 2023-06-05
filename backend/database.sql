c41fea010436c28305456316e2fbe44f72e4ffdb
zeitounmax <mthiry1@gmail.com>
2023-06-05 11:14:23 +0200

    First Commit

--- backend/database.sql
+++ backend/database.sql
@@ -111,4 +111,39 @@ Never miss a moment with the latest news, trending stories and highlights to bri
 
 ,('Victor Wembanyama claque un match de fou furieux, les USA sidérée!', 'Metropolitans 92 vs Ignite Highlights, Victor Wembanyama','https://www.youtube.com/watch?v=NtfIiFbQpG0', '4.38','2');
 
+ INSERT INTO `origins`.`video`('title', 'description', 'url', 'date', 'id_category') VALUES (' Carlos Alcaraz vs Lorenzo Musetti Rollercoaster Match! Hamburg 2022 Final Extended Highlights ','','https://www.youtube.com/watch?v=q77b-uFOiRc', '14.15', '3'), 
+ ,('Gael Monfils Top 5 ATP Matches Of His Career! 💪','https://www.youtube.com/watch?v=S9Y-7IbxZE0','15.45', '3')
+ ,(' Roland-Garros 2023 : le résumé de S. Ofner vs S. Tsitsipas ','https://www.youtube.com/watch?v=QfYaCeWzoLE','7.35', '3')
+ ,(' 🎾 Le résumé de Alexander Zverev 🇩🇪 v Frances Tiafoe 🇺🇸 ', 'https://www.youtube.com/watch?v=Abh-VeP6wmA','8.25', '3')
+ ,(' 🎾 Le résumé de Gael Monfils 🇫🇷 - Sebastián Báez 🇦🇷','https://www.youtube.com/watch?v=WfozbLPBIxg','22.59', '3' )
+ ,(' 🎾 Le résumé de Carlos Alcaraz 🇪🇸 v Denis Shapovalov 🇨🇦 ','https://www.youtube.com/watch?v=xIgRsioz0T8','6.28','3' )
+ ,(' Roland-Garros 2023 : le résumé de N.Djokovic vs D.Fokina','https://www.youtube.com/watch?v=QTOLVtH500A','9.58','3' ) 
+ ,(' Top 10 ATP Tennis Meltdowns & Angry Moments in 2020!','https://www.youtube.com/watch?v=yoLPLQDHR_E','17.45','3' )
+ ,('Daniil Medvedev Being Iconic For 15 Minutes 🤷‍♂️','https://www.youtube.com/watch?v=c0z6quY8iY8','15.16','3')
+ ,(' Epic Fails That Will Make You Feel Better About Your Tennis! 🤣','https://www.youtube.com/watch?v=NI3JdkzEOZI','8.54','3'),
+
+ INSERT INTO `origins`.`video`('title', 'description', 'url', 'date', 'id_category') VALUES (' Championnats d'Europe 2022 : De l'or pour les Bleus 🇫🇷 ','Le titre des Français sur le 4x100 m nage libre mixte, la médaille de Marie #Wattel ou le doublé de David #Popovici dans un temps canon : revivez les meilleurs moments de la cinquième journée des Championnats d Europe de natation de #Rome2022 (Italie)','https://www.youtube.com/watch?v=OSzOVLo2_Ao', '10.41', '4')
+ ,(' Championnats d'Europe 2022 : De l'argent 🥈 pour les relais ','C'est le clap de fin de ces championnats d'Europe de natation pour les Bleus ! Cette dernière journée de compétition s'est achevée avec de nouvelles médailles françaises : le bronze de Yohann Ndoye #Brouard sur le 100 m dos, ainsi que l'argent pour les relais masculins et féminins sur 4x 100 m 4 nages. La France achève ses championnats avec 17 médailles au compteurs : 3 en or, 7 en argent, 7 en bronze.','https://www.youtube.com/watch?v=c5WEdtH8mJ4','8.30','4' )
+ ,('  UNE JOURNÉE AVEC PHILIPPE LUCAS ET SON GROUPE À MARTIGUES - INSIDE ','De la sortie du domicile de Cyrielle Duhamel à 6h30 et à la fin du deuxième entrainement à 18h30, découvrez le quotidien des nageurs de Phillipe Lucas à Martigues','https://www.youtube.com/watch?v=gdgUAvMc3d4','14.33','4' )
+ ,('France Win Gold In 4x100m Freestyle Relay Final | London 2012 Olympics',' In an extremely close finish to the 4x100m mens freestyle France comeback to take the gold medal just ahead of USA and Russia','https://www.youtube.com/watch?v=-XI_XeoIFnI','8.59','4' )
+ ,(' Zhang Yufei - Womens 200 m butterfly | Reigning Champions ',' Zhang Yufei beams in delight on winning her second medal in the pool of the Tokyo 2020 Olympic Games, beating the American duo of Regan Smith and Hali Flickinger to silver and bronze, respectively. The Chinese went out fast in the women 200m butterfly winning in an Olympic Record time ,'https://www.youtube.com/watch?v=ArRwItJnoEk','2.06','4' )
+ ,(' Micheal Phelps Last Olympic Race-Swimming Men 4x100m Medley Relay Final', 'USA wins gold in the men's 4x100m medley final and set a new Olympic Record in Michael Phelps' final race at the Olympic Games.','https://www.youtube.com/watch?v=UmIYanq5gH8','8.36','4')
+ ,(' Men 200m Individual Medley',' USA's Michael Phelps wins his fourth consecutive gold in the Men's 200m Individual Medley.','https://www.youtube.com/watch?v=e-XGSYnhUjg','6.09','4')
+ ,(' USA Set New Women's 4 x 200m Freestyle Relay Olympic Record - London 2012 Olympics ',' The USA's team of Missy Franklin, Dana Vollmer, Shannon Vreeland and Allison Schmitt set a new Olympic record of 7:42.92 in the women's 4x200m freestyle relay event in the Aquatics Centre at the London 2012 Olympic Games (1 August).','https://www.youtube.com/watch?v=gALADAnOAhI','13.40', '4')
+ ,(' Sun Yang Smashes Men's 1500m Freestyle World Record - London 2012 Olympics ','https://www.youtube.com/watch?v=T5FlDy3YmDQ','20.52','4')
+ ,('Men 3m Springboard Final Rio 2016','Yuan Cao wins gold for China in men's 3m springboard Rio 2016.','https://www.youtube.com/watch?v=4ysjURAArmg','8.27','4'),
+
+ INSERT INTO `origins`.`video`('title', 'description', 'url', 'date', 'id_category') VALUES ('Golden Knights vs Canadiens-Faits Saillants','Voici des faits saillants du match qui opposait les Canadiens de Montréal aux Golden Knights de Vegas le 24 juin 2021.','https://www.youtube.com/watch?v=z-32F-CoIt4', '10.47', '5'),
+ ,('Panthers vs Canadiens-Faits Saillants,'Voici des faits saillants du match qui opposait les Canadiens de Montréal aux Panthers de la Floride le 29 avril 2022','https://www.youtube.com/watch?v=K_roXl1rnCQ','11.10','5')
+ ,('Montreal Canadiens Road To 2021 Stanley Cup Final','The Montreal Canadiens road to the 2021 NHL Stanley Cup Final was a cinderella story that will be remembered for a long time','https://www.youtube.com/watch?v=oCP6K7L5FV0','23.53','5')
+ ,('Re-Live The final Second of Each Stanley Cup Final FRom 2000-2021','Take a look back at the final moments of each Stanley Cup Final from 2000-2021','https://www.youtube.com/watch?v=YqAml6e-H7U','10.52','5')
+ ,('A historic Upset-Panthers vs Bruins','https://www.youtube.com/watch?v=vs51yCzVaI8','14.18','5')
+ ,('We Want Florida Panthers vs Maple Leafs','https://www.youtube.com/watch?v=NL1ZSmLYr30','12.27','5')
+ ,('NHL Controversial Shootout Moments','Nhl Controversial Shootout Moments. Check','https://www.youtube.com/watch?v=ll8bMh1I7oU','12.24','5')
+ ,('Switzerland vs Slovakia 2023-Highlights','Switzerland remains Group B’s only perfect team with 12 points after four games following a 4-2 victory over Slovakia.','https://www.youtube.com/watch?v=xHyPhgHMoCM','3.04','5')
+ ,('Canada vs Switzerland-Highlights','Damon Severson sent the game to OT with 0.4 seconds left, and Mark Stone did the rest in a remarkable comeback win for Canada over Switzerland','https://www.youtube.com/watch?v=JZv24iVczYI','2.32','5')
+ ,(' 🏆 Les Dragons de Rouen sacrés champions de France pour la 17ème fois !! ','Au terme d'un match très disputé, les Dragons de Rouen s'imposent en prolongation grâce à Dylan Yeo (4-3) et s'offrent un 17ème titre de champion de France. Rouen remporte la Synerglace Ligue Magnus 22/23 !','https://www.youtube.com/watch?v=uqi5EEhP3M4','8.40','5'),
+
+
+
 
