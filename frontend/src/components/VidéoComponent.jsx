5339df65cfa849801b117dfcbf46a642268ca9cf
JulienBenoit7 <julien-benoit@hotmail.fr>
2023-06-06 17:22:26 +0200

    create videoComponent for catch files to backend

---
+++ frontend/src/components/VidéoComponent.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+
+function VideoComponent() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3000"}/video`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setVideos(data);
+      })
+      .catch((error) => {
+        console.error(
+          "Une erreur s'est produite lors de la récupération des vidéos :",
+          error
+        );
+      });
+  }, []);
+
+  return (
+    <div>
+      {videos.map((video) => (
+        <div key={video.id}>
+          <p>{video.title}</p>
+          <video src={video.url} controls>
+            <track
+              kind="captions"
+              src={video.captionsUrl}
+              label="French"
+              default
+            />
+          </video>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default VideoComponent;
