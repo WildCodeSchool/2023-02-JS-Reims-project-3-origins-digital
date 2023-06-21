d41aac545e4788436b06b160aaa2ebb9a1372af3
zeitounmax <mthiry1@gmail.com>
2023-06-21 09:51:25 +0200

    Login CSS

--- frontend/src/pages/ForgetPass.jsx
+++ frontend/src/pages/ForgetPass.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "../App.css";
 
 function ForgetPass() {
   const [email, setEmail] = useState("");
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import { useRef } from "react";
+import React, { useRef } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
+import "../App.css";
 
 function Login() {
   const usernameRef = useRef();
@@ -9,50 +10,52 @@ function Login() {
   const { setToken } = useAuth();
   const navigate = useNavigate();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetch(
+      `${import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000"}/login`,
+      {
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({
+          username: usernameRef.current.value,
+          password: passwordRef.current.value,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setToken(data.token);
+        navigate("/");
+      });
+  };
+
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
+    <div className="container">
+      <img src="chemin/vers/logo.png" alt="Logo" />
 
-        fetch(
-          `${
-            import.meta.env.VITE_BACKEND_URL ?? "http://localhost:3000"
-          }/login`,
-          {
-            method: "post",
-            headers: {
-              "content-type": "application/json",
-            },
-            body: JSON.stringify({
-              username: usernameRef.current.value,
-              password: passwordRef.current.value,
-            }),
-          }
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            setToken(data.token);
+      <form onSubmit={handleSubmit}>
+        <div className="formulaire">
+          <label htmlFor="username">Nom d'utilisateur</label>
+          <input type="text" id="username" ref={usernameRef} />
 
-            navigate("/");
-          });
-      }}
-    >
-      <div className="formulaire">
-        <label htmlFor="username">Nom d'utilisateur</label>
-        <input ref={usernameRef} type="text" id="username" name="username" />
-        <label htmlFor="password">Mot de passe</label>
-        <input
-          ref={passwordRef}
-          type="password"
-          id="password"
-          name="password"
-        />
+          <label htmlFor="password">Mot de passe</label>
+          <input type="password" id="password" ref={passwordRef} />
 
-        <button type="submit">Connexion</button>
-        <Link to="/inscription">Inscription</Link>
-        <Link to="/forgetpass">Mot de passe oublié ?</Link>
-      </div>
-    </form>
+          <div className="controls">
+            <button type="submit">Connexion</button>
+
+            <div className="links">
+              <Link to="/inscription">Inscription</Link>
+              <Link to="/forgetpass">Mot de passe oublié ?</Link>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
