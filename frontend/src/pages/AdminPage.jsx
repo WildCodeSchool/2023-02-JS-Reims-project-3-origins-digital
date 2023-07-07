9e2fd4fa347d80a3baa126af22164160eba9447c
Florian GERON <geron-f@outlook.fr>
2023-07-07 15:26:45 +0200

    ajout du composant Addvideo

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { FaTrash } from "react-icons/fa";
-import { LuEdit } from "react-icons/lu";
+import { FaTrash, FaEdit } from "react-icons/fa";
 import { AiFillFileAdd, AiFillHome } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 function AdminPage() {
   const [message, setMessage] = useState("");
@@ -10,36 +10,22 @@ function AdminPage() {
     setMessage("Accueil");
   };
 
-  const addVideo = () => {
-    setMessage("Vidéo ajoutée !");
-  };
-
-  const deleteVideo = () => {
-    // Code pour supprimer une vidéo
-    setMessage("Vidéo supprimée !");
-  };
-
-  const editVideo = () => {
-    // Code pour modifier une vidéo
-    setMessage("Vidéo modifiée !");
-  };
-
   return (
     <>
-      <h1>Page administateur</h1>
+      <h1>Page administrateur</h1>
       <div className="buttonNav">
         <button type="button" onClick={accueil}>
           <AiFillHome size={50} color="black" /> Accueil
         </button>
-        <button type="button" onClick={addVideo}>
-          <AiFillFileAdd size={50} color="black" /> Ajouté
-        </button>
-        <button type="button" onClick={editVideo}>
-          <LuEdit size={50} color="black" /> Modifié
-        </button>
-        <button type="button" onClick={deleteVideo}>
-          <FaTrash size={50} color="black" /> Supprimé
-        </button>
+        <Link to="/addvideos" className="linkButton">
+          <AiFillFileAdd size={50} color="black" /> Ajouter une vidéo
+        </Link>
+        <Link to="/editvideos" className="linkButton">
+          <FaEdit size={50} color="black" /> Modifier une vidéo
+        </Link>
+        <Link to="/deletevideos" className="linkButton">
+          <FaTrash size={50} color="black" /> Supprimer des vidéos
+        </Link>
       </div>
 
       {message && <p>{message}</p>}
